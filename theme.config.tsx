61fbffde14afc14cd290f20f051381ff9fee8528
AlandSleman <alandsazkm@gmail.com>
2023-06-03 10:07:38 +0300

    docs structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Next.js 13 Guide</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/AlandSleman/next-guide/tree/master',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/AlandSleman/next-guide/tree/master',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Next.js 13 Guide',
   },
 }
 
