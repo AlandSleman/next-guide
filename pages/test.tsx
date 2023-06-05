cfcbc93d2409e9d51335c6637e038734993978af
AlandSleman <alandsazkm@gmail.com>
2023-06-05 22:50:09 +0300

    nextra requires css modules

---
+++ pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Tree from '../components/test-file-tree'
+
+const test = (props : {}) => {
+  return (
+    <div>
+      <Tree/>
+    </div>
+  )
+}
+export default test
