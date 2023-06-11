809971d9c3c453a4c5e03646ed28cdf5fd539bdd
AlandSleman <alandsazkm@gmail.com>
2023-06-11 04:09:58 +0300

    add Plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import '@/styles/globals.css'
+import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
- 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
-      <Analytics />
+      <PlausibleProvider domain="kurdmake.com">
+        <Component {...pageProps} />
+        <Analytics />
+      </PlausibleProvider >
     </>
   );
 }
- 
+
 export default MyApp;
